7fafbb7dc9f4e04a6e830732bca651261674dc26
lrj-sorry <3575766578@qq.com>
2026-04-13 14:01:30 +0800

    chore: switch to 温馨 playlist (439928756, 112首)

--- js/music.js
+++ js/music.js
@@ -12,7 +12,7 @@ GitHub：https://github.com/imsyy/home
 */
 let server = "netease"; //netease: 网易云音乐; tencent: QQ音乐; kugou: 酷狗音乐; xiami: 虾米; kuwo: 酷我
 let type = "playlist"; //song: 单曲; playlist: 歌单; album: 唱片
-let id = "7455421800"; //封面 ID / 单曲 ID / 歌单 ID (网易云歌单，35首)
+let id = "439928756"; //封面 ID / 单曲 ID / 歌单 ID (纯音/治愈/温馨BGM，112首)
 
 $.ajax({
     url: "https://api.injahow.cn/meting/?server=" + server + "&type=" + type + "&id=" + id,
