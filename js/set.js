215c6b617feb34ec3cf64b84d8362f40d33fed3a
lrj-sorry <3575766578@qq.com>
2026-04-14 22:46:08 +0800

    fix: 替换失效的壁纸API (api.ixiaowai.cn -> bing.img.run)

--- js/set.js
+++ js/set.js
@@ -82,8 +82,8 @@ function getBgImg() {
 let bg_img_preinstall = {
     "type": "1", // 1:默认背景 2:每日一图 3:随机风景 4:随机动漫
     "2": "https://api.dujin.org/bing/1920.php", // 每日一图
-    "3": "https://api.ixiaowai.cn/gqapi/gqapi.php", // 随机风景
-    "4": "https://api.ixiaowai.cn/api/api.php" // 随机动漫
+    "3": "https://bing.img.run/1920x1080.php?type=nature", // 随机风景
+    "4": "https://bing.img.run/1920x1080.php?type=anime" // 随机动漫
 };
 
 // 更改背景图片
